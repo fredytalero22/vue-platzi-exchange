0f5433854811d30cb37475ca17037c43d3718a35
Fredy Talero <fredy.talero@gmail.com>
2020-05-12 00:19:31 -0500

    Proyecto terminado de Vue.js

---
+++ src/components/PxButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    @click="buttonClick"
+    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-2 border border-blue-500 hover:border-transparent rounded"
+  >
+    <beat-loader :loading="isLoading" :colors="'#63b3ed'" :size="8" />
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PxButton",
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    buttonClick() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <pacman-loader :loading="isLoading" :color="'#63b3ed'" :size="50" />
+    <px-assets-table v-if="!isLoading" :assets="assets" />
+  </div>
+</template>
+
+<script>
+import api from "@/api";
+import PxAssetsTable from "@/components/PxAssetsTable";
+
+export default {
+  name: "Home",
+  components: { PxAssetsTable },
+  data() {
+    return {
+      isLoading: false,
+      assets: []
+    };
+  },
+
+  created() {
+    this.isLoading = true;
+    api
+      .getAssets()
+      .then(assets => (this.assets = assets))
+      .finally(() => (this.isLoading = false));
+  }
+};
+</script>
